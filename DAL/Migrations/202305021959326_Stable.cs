861d1d701ca2d307d98d8ae6a4948a926a832614
ShariarMiraj <shahriarmiraj116@gmail.com>
2023-05-03 02:10:58 +0600

    acbc

--- DAL/Migrations/202305021959326_Stable.cs
+++ DAL/Migrations/202305021959326_Stable.cs
@@ -3,7 +3,7 @@
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class alltables : DbMigration
+    public partial class Stable : DbMigration
     {
         public override void Up()
         {
