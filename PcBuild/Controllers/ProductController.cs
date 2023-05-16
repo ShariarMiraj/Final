082866a39664161902bfaa1ac03387f66361218a
ShariarMiraj <shahriarmiraj116@gmail.com>
2023-05-16 12:16:31 +0600

    puullall

--- PcBuild/Controllers/ProductController.cs
+++ PcBuild/Controllers/ProductController.cs
@@ -6,9 +6,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace PcBuild.Controllers
 {
+
+    [EnableCors("*", "*", "*")]
+
     public class ProductController : ApiController
     {
         [HttpGet]
--- PcBuild/Controllers/SellerController.cs
+++ PcBuild/Controllers/SellerController.cs
@@ -6,9 +6,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace PcBuild.Controllers
 {
+    [EnableCors("*", "*", "*")]
     public class SellerController : ApiController
     {
         [HttpGet]
